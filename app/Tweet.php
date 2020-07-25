d64ee9a7a6438869ecc60ad1741d146bc88a4e7a
Marin Varnica <marinvarnica@gmail.com>
2020-07-25 10:20:20 +0200

    deleted follower and added tweet table

--- app/Tweet.php
+++ app/Tweet.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Follower extends Model
+class Tweet extends Model
 {
-    protected $guarded = [];
+    //
 }
--- database/migrations/2020_07_25_081357_create_tweets_table.php
+++ database/migrations/2020_07_25_081357_create_tweets_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFollowersTable extends Migration
+class CreateTweetsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,13 @@ class CreateFollowersTable extends Migration
      */
     public function up()
     {
-        Schema::create('followers', function (Blueprint $table) {
+        Schema::create('tweets', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id')->index();
-            $table->unsignedBigInteger('following_id')->index();
+            $table->text('body');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
@@ -28,6 +30,6 @@ class CreateFollowersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('followers');
+        Schema::dropIfExists('tweets');
     }
 }
